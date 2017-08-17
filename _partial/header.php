c1cea8eae3acea6caa8fcee6b611734cc54c6bed
Akash Chavan <achavan445@gmail.com>
2017-08-17 12:50:20 +0530

    upload

---
+++ _partial/header.php
@@ -0,0 +1,19 @@
+<div class="header_div">
+    <div class="container p-lr-xxl p-tb-xs">
+        <div class="logo-div pull-left col-sm-3">
+            <a href="index.php"><img src="img/logo.png" class="logo_img" alt="trusha patel astrology" title="Astrogain" /></a>
+        </div>
+        <div class="navi_div col-sm-9">
+            <nav class="pull-right">
+                <ul class="navigation">
+                    <li><a href="#">Home</a><span class="hover_arrow"></span></li>
+                    <li><a href="#">About Me</a><span class="hover_arrow"></span></li>
+                    <li><a href="#">Services</a><span class="hover_arrow"></span></li>
+                    <li><a href="#">Make Oppointment</a><span class="hover_arrow"></span></li>
+                    <li><a href="#" style="padding-right:0px;">Contact Me</a><span class="hover_arrow"></span></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="clearfix"></div>
+    </div>
+</div>
